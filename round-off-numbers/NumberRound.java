fe58d7ba2286ad67077058aded760ba14691c2b1
gcastill0 <gilberto.castillo@rogers.com>
2024-09-12 14:31:06 -0400

    Adding number precision handling

--- round-off-numbers/NumberRound.java
+++ round-off-numbers/NumberRound.java
@@ -8,9 +8,17 @@ public class NumberRound {
         double PI2 = Math.round(PI * 100.0) / 100.0;
 
         System.out.printf("\n\n");
+
+        // PI 3.141593 is the original
         System.out.printf( "PI %f is the original\n", PI);
+
+        // PI2 3.140000 is the calculated number to two decimal precision places
+
         System.out.printf( "PI2 %f is the calculated number to two decimal precision places\n", PI2);
+
+        // PI2 3.14 displays to two decimal places
         System.out.printf( "PI2 %.2f displays to two decimal places\n", PI2);
+        
         System.out.printf("\n\n");
 
     }
