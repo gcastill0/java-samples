478d70d548e70961e87add79a611c15666cf7800
gcastill0 <gilberto.castillo@rogers.com>
2025-10-23 19:12:09 -0400

    Updating documentation

--- odd-numbers/IntegerDivisionExample.java
+++ odd-numbers/IntegerDivisionExample.java
@@ -1,14 +1,31 @@
 public class IntegerDivisionExample {
 
+    /** Determine if a number is even 
+     * @param number is the value to evaluate
+     * 
+     * @return true if the number is even, 
+     * false is the number is odd
+     * 
+     * Use the logic of number - [ ( number / 2 ) * 2 )
+     * where the division of an integer truncates
+     * the value. For instance, given a number = 5, 
+     * 5 - ((5/2) * 2) -> 5 - (2 * 2) -> 5 - 4 -> 1.
+     * 
+     * Also, number = 4 
+     * 4 - ((4/2) * 2) -> 4 - (2*2) -> 4 - 4 -> 0
+     * 
+     * If the value difference is zero, then the
+     * number is even. Otherwise, the number is odd.
+    */
     public static boolean isEven(int number) {
 
         int half = number / 2;
         int whole = half * 2;
         int diff = number - whole;
 
-        if (diff == 1) return false;
+        if (diff == 0) return true;
 
-        return true;
+        return false;
     }
 
     public static void main(String[] args) {
@@ -16,7 +33,13 @@ public class IntegerDivisionExample {
         int[] numbers  = { 7, 2, 5, 6, 1, 3, 4 };
 
         for (int number: numbers) {
-            System.out.println("Is " + number + " even? -> " + isEven(number));
+
+            if (isEven(number)) {
+              System.out.println(number + " is even");
+            } else {
+              System.out.println(number + " is odd");
+            }
+
         }
     }
 }
