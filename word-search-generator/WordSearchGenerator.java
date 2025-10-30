a4026a19ea5cdb27bc7a191f62a95108dba3e187
gcastill0 <gilberto.castillo@rogers.com>
2025-10-30 18:30:05 -0400

    Code: Adding method for file input

--- word-search-generator/WordSearchGenerator.java
+++ word-search-generator/WordSearchGenerator.java
@@ -1,23 +1,41 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class WordSearchGenerator {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-            String fileName = "input.txt"; 
-            
-            try (
-                 FileReader fileInput = new FileReader(fileName);
-                 BufferedReader reader = new BufferedReader(fileInput)) {
-                String line;
-                while ((line = reader.readLine()) != null) {
+        String fileName = getFileName();
+
+        try (
+                FileReader fileInput = new FileReader(fileName);
+                BufferedReader reader = new BufferedReader(fileInput)) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
                     System.out.println(line);
                 }
-            } catch (IOException e) {
-                System.err.println("Error reading file: " + e.getMessage());
-            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
         }
-        
+    }
+
+    public static String getFileName() throws IOException {
+
+        String defaultFileName = "input.txt";
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            System.out.print("Please enter the file name: ");
+            String fileName = br.readLine();
+            String userFileName = ( fileName == null || fileName.trim().isEmpty() ) ? defaultFileName : fileName;
+            return userFileName ;
+        } catch (IOException e) {
+            System.err.println("Error getting file name: " + e.getMessage());
+            return defaultFileName;
+        }
+    }
+
 }
