db6639d54adf92d6b0fe391f22cb5efacb4cb27a
gcastill0 <gilberto.castillo@rogers.com>
2025-10-30 19:30:43 -0400

    Code: Adding to read input methods

---
+++ word-search-generator/CustomFileException.java
@@ -0,0 +1,5 @@
+public class CustomFileException extends Exception {
+    public CustomFileException(String message) {
+        super(message);
+    }
+}
--- word-search-generator/WordSearchGenerator.java
+++ word-search-generator/WordSearchGenerator.java
@@ -7,38 +7,59 @@ public class WordSearchGenerator {
 
     public static void main(String[] args) throws IOException {
 
-        String fileName = getFileName();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String fileName = readFileName("Please enter the file name: ", br);
+        Integer rows    = readIntNumber("Enter the number of rows: ", br);
+        Integer cols    = readIntNumber("Enter the number of columns: ", br);
 
         try (
                 FileReader fileInput = new FileReader(fileName);
                 BufferedReader reader = new BufferedReader(fileInput)) {
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        System.out.println(line);
-                    } 
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
         } catch (IOException e) {
             System.err.println("Error reading file: " + e.getMessage());
         }
     }
 
-    public static String getFileName() throws IOException {
+    public static String readFileName(String message, BufferedReader br) throws IOException {
         String defaultFileName = "input.txt";
 
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
-            System.out.print("Please enter the file name: ");
+        try {
+            System.out.print(message);
             String fileName = br.readLine();
-            
-            if ( fileName == null || fileName.trim().isEmpty() ) {
+
+            if (fileName == null || fileName.trim().isEmpty()) {
                 System.out.println("File is empty. Using default file " + defaultFileName);
                 fileName = defaultFileName;
             }
-            
-            return fileName ;
+
+            return fileName;
 
         } catch (IOException e) {
             System.err.println("Error getting file name: " + e.getMessage());
             return defaultFileName;
         }
+    }
+
+    public static int readIntNumber(String message, BufferedReader br) throws IOException {
+
+        int number = -99;
+
+        while (true) {
+            try {
+                System.out.print(message);
+                number = Integer.parseInt(br.readLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid whole number.");
+            }
+        }
+
+        return number;
     }
 
 }
